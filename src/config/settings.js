ed8f6408ff1be1dd8bd543d26813cca5a33c9a25
Adina Pusok <adina.pusok@gmail.com>
2025-05-23 11:21:32 +0100

    Update seminar speakers.

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Seafloor Explorer | Online Seminar Series`,
-	description: `Seminar series focused on oceanic tectonics - geophysics, geochemistry, geology - and associated bio-geological processes.`,
+	description: `Seminar series focused on ocean tectonics - geophysics, geochemistry, geology - and associated bio-geological processes.`,
 	// url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `Seafloor Explorer`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
