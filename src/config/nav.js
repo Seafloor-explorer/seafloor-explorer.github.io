a133337b92dad4869f1de4753720fb34b439509e
Adina Pusok <adina.pusok@gmail.com>
2026-01-21 12:04:08 +0000

    Added speaker suggestions to nav

--- src/config/nav.js
+++ src/config/nav.js
@@ -7,6 +7,10 @@ export const nav = [
 		title: 'Join mailing list',
 		slug: 'https://www.jiscmail.ac.uk/cgi-bin/wa-jisc.exe?SUBED1=SEAFLOOR-EXPLORER&A=1',
 	},
+	{
+		title: 'Suggest speakers',
+		slug: 'https://docs.google.com/forms/d/e/1FAIpQLSeXyoUFaV6iUfiO5yNHFVOBwJq94f062mlj2-GaVmHUnsLoQg/viewform?usp=header',
+	},
 	// {
 	// 	title: 'Subscribe',
 	// 	slug: '/seafloor/contact',
